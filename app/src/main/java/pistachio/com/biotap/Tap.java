a15d3feab968dee57787dd5acbec5d33568a319c
cameronDz <cameron.dziurgot@my.ccsu.edu>
2015-11-12 20:18:58 -0500

    Started Compare class for running algorithm.

--- app/src/main/java/pistachio/com/biotap/Tap.java
+++ app/src/main/java/pistachio/com/biotap/Tap.java
@@ -1,68 +1,55 @@
 package pistachio.com.biotap;
 
 public class Tap {
+    /**/
+    /*The time variable needs to be changed to the difference*/
+    /* between down press and up press for the algorithm.*/
+    /**/
+    private long time;  /*down press timestamp*/
+    private float x;    /*x coordinate*/
+    private float y;    /*y coordinate*/
 
-    /**
-     * Timestamp
-     */
-    private long time;
-
-    /**
-     * x coordinate
-     */
-    private float x;
-
-    /**
-     * y coordinate
-     */
-    private float y;
-
-    /**
-     * Create a new instance of a tap.
-     *
-     * @param time
-     * @param x
-     * @param y
-     */
+    /*Create a new instance of a tap.*/
+    /* @param time */
+    /* @param x */
+    /* @param y */
     private Tap(long time, float x, float y) {
-
         this.set(time, x, y);
     }
 
-    /**
-     * Named constructor for a tap.
-     *
-     * @param time
-     * @param x
-     * @param y
-     *
-     * @return Tap
-     */
-    public static Tap record(long time, float x, float y) {
+    /*Named constructor for a tap.*/
+    /* @param time */
+    /* @param x */
+    /* @param y */
+    /* @return Tap */
+    public static Tap record(long time, float x, float y){
         return new Tap(time, x, y);
     }
 
-    /**
-     * Set properties of at tap.
-     *
-     * @param time
-     * @param x
-     * @param y
-     */
+    /*Set properties of at tap.*/
+    /* @param time */
+    /* @param x */
+    /* @param y */
     protected void set(long time, float x, float y) {
-
         this.time = time;
         this.x = x;
         this.y = y;
     }
 
-    /**
-     * Convert object to string.
-     *
-     * @return String
-     */
+    /*Convert object to string.*/
+    /* @return String */
     public String toString() {
         return this.time + "/" + this.x + "/" + this.y;
     }
 
+    /*Getters. Used for comparison algorithm.*/
+    public float getX() {
+        return this.x;
+    }
+    public float getY() {
+        return this.y;
+    }
+    public long getTime() {
+        return this.time;
+    }
 }
