2d6693de21e05dddbe30855f69e0b32cd910ebd3
donnyray <travis.moretz@gmail.com>
2015-11-16 20:52:19 -0500

    start of the average passkey, adds class, writing to file, updates Taps for averaging sequences together

---
+++ app/src/main/java/pistachio/com/biotap/AvgPassKeyActivity.java
@@ -0,0 +1,13 @@
+package pistachio.com.biotap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvgPassKeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avg_pass_key);
+    }
+}
