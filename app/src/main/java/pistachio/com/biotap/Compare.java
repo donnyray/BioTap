0db6951a4278237bdbb266789e01372dc2e2d922
cameronDz <cameron.dziurgot@my.ccsu.edu>
2015-11-19 18:22:09 -0500

    Update Compare.java
    Works. Returns score.

--- app/src/main/java/pistachio/com/biotap/Compare.java
+++ app/src/main/java/pistachio/com/biotap/Compare.java
@@ -5,16 +5,17 @@
 /*  sequence. Also has methods that calulate the standard deviation*/
 /*  between a given number of sequences, which returns modified    */
 /*  values in a Tap object for convience of passing data.          */
+/* @version(11 Nov 2015) Tested AlgoTest.Java, returns a dis score.*/
 package pistachio.com.biotap;
 
+
 import java.util.ArrayList;
-import java.util.Math;
 
 public class Compare {
 
     /*Class used for comparing 2 tap sequences. Returns a score by*/
     /* comparing the master and attempted sequence.*/
-    public double dissimilarityScore(ArrayList<Tap> master,
+    static public double dissimilarityScore(ArrayList<Tap> master,
                                      ArrayList<Tap> attempt,
                                      ArrayList<Tap> standDev) {
         /*Checks to make sure sequences are the same size. If not*/
@@ -39,21 +40,19 @@ public class Compare {
     
     
     /*Breaks down Taps by their elements and performs formula.*/
-    private double tapFormula(Tap master, Tap attempt, Tap sd) {
+    static private double tapFormula(Tap master, Tap attempt, Tap sd) {
         double total = 0.0;
-        total += longTapForm(master.getTime(),
-                             attempt.getTime(),
-                             sd.getTime());
-        total += longTapForm(master.getDur(),
-                             attempt.getDur()
-                             sd.getDur());
+        total += longTapForm(master.getTime(),attempt.getTime(), sd.getTime());
+        total += longTapForm(master.getDuration(),
+                             attempt.getDuration(),
+                             sd.getDuration());
         total += intTapForm(master.getX(),
                             attempt.getX(),
                             sd.getX());
         total += intTapForm(master.getY(),
                             attempt.getY(),
                             sd.getY());
-        }
+        
         return total;
     }
 
@@ -62,59 +61,67 @@ public class Compare {
     
     /*Takes an array of Tap sequences and comes up with a standard*/
     /* deviation tap sequence.*/
-    public ArrayList<Tap> standDeviation(int trials,
+    static public ArrayList<Tap> standDeviation(int trials,
                                          ArrayList<Tap>[] sequences) {
         /*Sequence to be returned.*/
-        ArrayList<Tap> sd = new ArrayList<Tap>;
+        ArrayList<Tap> sd = new ArrayList<>();
         /*Number of taps in each sequence.*/
         int numTaps = sequences[0].size();
+        /*System.out.print("TEST numTaps:" + sequences[0].size() +"\n");*/
+        /*System.out.print("TEST trials:" + trials +"\n");*/
         /*Loop checks to make sure all sequences are same length.*/
-        for (int i = 1; i < trials; i++) {
-            if (numTaps != sequences[trials].size()) {
+        for (int i = 0; i < trials; i++) {
+            /*System.out.print("TEST i1:" + i +"\n");*/
+            if (numTaps != sequences[i].size()) {
                 /*THROW AN ERROR INSTEAD OF RETURNING EMPTY LIST*/
+                /*System.out.print("TEST i2:" + i +"\n");*/
+                /*System.out.print("TEST seq[" + i + "]: " + sequences[i].toString() + "\n");*/
                 return sd;
             }
         }
+        
         /*Mean of the sequences.*/
-        ArrayList<Tap> mean = Compare.meanSeq(trails, sequences);
+        ArrayList<Tap> mean = Compare.meanSeq(trials, sequences);
+        /*System.out.print("TEST mean size: " + mean.size() + "\n");*/
         
         /*Calculates standard deviation.*/
         /*Looping through each tap in all sequences.*/
         for (int a = 0; a < numTaps; a++) {
             /*Sum of the difference of each trial element minus*/
             /* the mean element, squared.*/
-            long timeSum = 0.0;
-            long durSum = 0.0;
-            int xSum = 0.0;
-            int ySum = 0.0;
+            long timeSum = 0;
+            long durSum = 0;
+            int xSum = 0;
+            int ySum = 0;
             /*Sums the element difference square in each sequence.*/
             for (int b = 0; b < trials; b++) {
-                timeSum += ((sequence[b].get(a).getTime() -
+                /*System.out.print("TEST a: " + a + " b: " + b + "\n");*/
+                timeSum += ((sequences[b].get(a).getTime() -
                              mean.get(a).getTime()) *
-                            (sequence[b].get(a).getTime() -
+                            (sequences[b].get(a).getTime() -
                              mean.get(a).getTime()));
-                durSum  += ((sequence[b].get(a).getDur() -
-                             mean.get(a).getDur()) *
-                            (sequence[b].get(a).getDur() -
-                             mean.get(a).getDur()));
-                xSum    += ((sequence[b].get(a).getX() -
+                durSum  += ((sequences[b].get(a).getDuration() -
+                             mean.get(a).getDuration()) *
+                            (sequences[b].get(a).getDuration() -
+                             mean.get(a).getDuration()));
+                xSum    += ((sequences[b].get(a).getX() -
                              mean.get(a).getX()) *
-                            (sequence[b].get(a).getX() -
+                            (sequences[b].get(a).getX() -
                              mean.get(a).getX()));
-                ySum    += ((sequence[b].get(a).getY() -
+                ySum    += ((sequences[b].get(a).getY() -
                              mean.get(a).getY()) *
-                            (sequence[b].get(a).getY() -
+                            (sequences[b].get(a).getY() -
                              mean.get(a).getY()));
             }
             /*Standard deviations formula in double form.*/
             double time = (Math.sqrt(timeSum))/trials;
-            double dur = (Math.sqrt(durTime))/trials;
+            double dur = (Math.sqrt(durSum))/trials;
             double x = (Math.sqrt(xSum))/trials;
             double y = (Math.sqrt(ySum))/trials;
             /*Cast variables so they can be stored in a Tap object.*/
             /* Creates Tap and put into stand dev sequence.*/
             Tap castedTap = new Tap(dblToLong(time), dblToLong(dur),
-                                    dblToInt(x), dlbToInt(y));
+                                    dblToInt(x), dblToInt(y));
             sd.add(castedTap);
         }
         /*Send back standard deviation array.*/
@@ -122,8 +129,9 @@ public class Compare {
     }
     
     /*Returns an array that is the mean of sequences.*/
-    public ArrayList<Tap> meanSeq(int trial, ArrayList<Tap>[] seq) {
-        ArrayList<Tap> mean = new ArrayList<Tap>;
+    public static ArrayList<Tap> meanSeq(int trial, ArrayList<Tap>[] seq) {
+        ArrayList<Tap> average = new ArrayList<>();
+        int numTaps = seq[0].size();
         /*Loops through each Tap in all sequences.*/
         for (int k = 0; k < numTaps; k++) {
             /*Sum of each element of a Tap at kth Tap in sequence.*/
@@ -133,28 +141,34 @@ public class Compare {
             int ySum = 0;
             /*Creates a running sum of all the elements of the kth*/
             /* tap in a sequence.*/
-            for (int j = 0; j < trials; j++) {
-                timeSum += sequences[j].get(k).getTime();
-                durSum  += sequences[j].get(k).getDuration();
-                xSum    += sequences[j].get(k).getX();
-                ySum    += sequences[j].get(k).getY();
+            for (int j = 0; j < trial; j++) {
+                /*System.out.print("TEST j: " + j + " k: " + k + "\n");*/
+                timeSum += seq[j].get(k).getTime();
+                /*System.out.print("TEST timeSum: " + timeSum + "\n");*/
+                durSum  += seq[j].get(k).getDuration();
+                /*System.out.print("TEST durSum: " + durSum + "\n");*/
+                xSum    += seq[j].get(k).getX();
+                /*System.out.print("TEST xSum: " + xSum + "\n");*/
+                ySum    += seq[j].get(k).getY();
+                /*System.out.print("TEST ySum: " + ySum + "\n");*/
+                /*System.out.print("TEST seq[" + j + "]: " + seq[j].toString() + "\n");*/
             }
             /*Create an average tap, add it to the mean sequence.*/
-            Tap ave = new Tap((long)(timeSum/trials),
-                              (long)(durSum/trials),
-                              (xSum/trials),
-                              (ySum/trials));
-            mean.add(ave);
+            Tap ave = new Tap((long)(timeSum/trial),
+                              (long)(durSum/trial),
+                              (xSum/trial),
+                              (ySum/trial));
+            average.add(ave);
         }
         /*Return average.*/
-        return mean;
+        return average;
     }
     
     
     /*Dissimilarity score formula broken down by long and int.*/
     /* Differentiated because of the way stand dev is stored in*/
     /* Tap object. If statement checks for division by 0.*/
-    private double longTapForm(long mast, long att, long sd) {
+    static private double longTapForm(long mast, long att, long sd) {
         double score = 0.0;
         if (sd == 0) {
             return score;
@@ -162,12 +176,12 @@ public class Compare {
         score = (double)(Math.abs(att-mast)/longToDbl(sd));
         return score;
     }    
-    private double intTapForm(int mast, int att, int sd) {
+    static private double intTapForm(int mast, int att, int sd) {
         double score = 0.0;
         if (sd == 0) {
             return score;
         }
-        score = (double)(Math.abs(att-mast)/intToDble(sd));
+        score = (double)(Math.abs(att-mast)/intToDbl(sd));
         return score;
     }
     /*Methods used in converting back and forth to doubles for*/
@@ -175,16 +189,16 @@ public class Compare {
     /* deviation is multiplied by 1000 to be put into the Tap, and*/
     /* divided by 1000 when being extracted from a Tap to maintain*/
     /* precision.*/
-    private long dblToLong(double d) {
+    static private long dblToLong(double d) {
         return (long)(d*1000);
     }
-    private int dblToInt(double d) {
+    static private int dblToInt(double d) {
         return (int)(d*1000);
     }
-    private double longToDbl(long l) {
+    static private double longToDbl(long l) {
         return (double)l/1000.0;
     }
-    private double intToDbl(int i) {
+    static private double intToDbl(int i) {
         return (double)i/1000.0;
     }
 }
